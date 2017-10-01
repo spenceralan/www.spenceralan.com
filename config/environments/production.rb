ce41e93b284c0944df54fba26012c1d54b25775d
Spencer Alan <spencer.alan@icloud.com>
2017-10-01 09:07:54 -0700

    add paperclip configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,16 @@ Rails.application.configure do
 
   # force HTTPS on production
   config.force_ssl = true
+
+  #paperclip S3 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV["AWS_S3_REGION"],
+    s3_credentials: {
+      s3_host_name: ENV["AWS_S3_HOST_NAME"],
+      bucket: ENV["AWS_S3_BUCKET"],
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    }
+  }
 end
